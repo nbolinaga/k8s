b131dbbe1efff65d51dd263a3a1fe94a7259fcb2
nbolinaga18 <nbolinaga17@gmail.com>
2023-03-09 19:16:50 -0400

    added yaml files

--- apps/admin/src/pages/index.tsx
+++ apps/admin/src/pages/index.tsx
@@ -1,34 +1,23 @@
-export default function Home(props: { ip: any; }) {
-  const ip = props.ip;
+import { useState, useEffect } from 'react'
+
+export default function Hostname() {
+  const [hostname, setHostname] = useState('Loading...')
+
+  useEffect(() => {
+    const fetchHostname = async () => {
+      const res = await fetch('/api/hello')
+      const data = await res.json()
+      setHostname(data.hostname)
+    }
+
+    fetchHostname()
+  }, [])
+
   return (
-    <div style={{ width: '100vw', height: '100vh', padding: 30 }}>
-      <h1>
-        ADMIN PAGE
-      </h1>
-      <h2>
-        IP Address <span style={{ color: 'blue' }}>{ip}</span>
-      </h2>
+    <div>
+      <h1> ADMIN PAGE </h1>
+      <h2>Hostname</h2>
+      <p>{hostname}</p>
     </div>
-  );
-}
-
-export async function getServerSideProps(context: { req: any; }) {
-  let ip;
-
-  const { req } = context;
-
-  if (req.headers['x-forwarded-for']) {
-    ip = req.headers['x-forwarded-for'].split(',')[0];
-  } else if (req.headers['x-real-ip']) {
-    ip = req.connection.remoteAddress;
-  } else {
-    ip = req.connection.remoteAddress;
-  }
-
-  console.log(ip);
-  return {
-    props: {
-      ip,
-    },
-  };
+  )
 }
