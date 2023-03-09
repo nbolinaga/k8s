19d378de80202678ee737b884a711eb9227e9333
nbolinaga18 <nbolinaga17@gmail.com>
2023-03-09 19:31:41 -0400

    fixed error in api

--- apps/client/src/pages/api/hello.ts
+++ apps/client/src/pages/api/hello.ts
@@ -1,4 +1,4 @@
-export default function handler(req, res) {
+export default function handler(req: { headers: any }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { hostname: any }): void; new(): any } } }) {
   const { headers } = req
   const hostname = headers.host
   res.status(200).json({ hostname })
