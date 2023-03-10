6da085a87058733cf8afb5e840d159dd0aead349
nbolinaga18 <nbolinaga17@gmail.com>
2023-03-09 20:18:56 -0400

    fixed hostname

--- apps/client/src/pages/api/hello.ts
+++ apps/client/src/pages/api/hello.ts
@@ -1,5 +1,9 @@
-export default function handler(req: { headers: any }, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { hostname: any }): void; new(): any } } }) {
+export default function handler(req: {
+  connection: any; headers: any 
+}, res: { status: (arg0: number) => { (): any; new(): any; json: { (arg0: { hostname: any }): void; new(): any } } }) {
   const { headers } = req
-  const hostname = headers.host
+  const hostname = headers['x-forwarded-for'] || headers['x-real-ip'] || req.connection.remoteAddress
+  
   res.status(200).json({ hostname })
+  
 }
